509a235092f206ac4b7946508b1648ef17a8fc31
Gathin23 <gathint23@gmail.com>
2022-06-22 09:11:13 +0530

    Created tasks component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ const App = () => {
       id: 1,
       text: "Doctor appointment",
       day: "Feb 5th at 2:30pm",
+      remider: true,
+    },
+    {
+      id: 2,
+      text: "Meeting in school",
+      day: "Feb 6th at 1:30pm",
+      remider: true,
+    },
+    {
+      id: 1,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      remider: false,
     },
   ]);
   return (
