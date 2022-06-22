377ea9f6d5bd60850ff6bedbd80fe42e0efe4286
Gathin23 <gathint23@gmail.com>
2022-06-22 15:44:51 +0530

    Completed the UI part of the website

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,54 @@
-const AddTask = () => {
+import { useState } from "react";
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text) {
+      alert("Please add a task");
+    }
+
+    onAdd({ text, day, reminder });
+    //resetting again
+    setText("");
+    setDay("");
+    setReminder(false);
+  };
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={onSubmit}>
       <div className="form-control">
         <label>Task</label>
-        <input type="text" placeholder="Add Task" />
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
       <div className="form-control">
         <label>Day & Time</label>
-        <input type="text" placeholder="Add Day & Time" />
+        <input
+          type="text"
+          placeholder="Add Day & Time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
       </div>
-      <div className="form-control">
-        <label>Set Remainder</label>
-        <input type="checkbox" />
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
       </div>
-      <input type="submit" value="Save Task" />
+      <input className="btn btn-block" type="submit" value="Save Task" />
     </form>
   );
 };
