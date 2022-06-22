49433a2b459e5906c3173f9cd211dd14ac8dffa4
Gathin23 <gathint23@gmail.com>
2022-06-22 10:28:04 +0530

    Created form and completed delete and remainder for tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTasks from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -8,25 +9,45 @@ const App = () => {
       id: 1,
       text: "Doctor appointment",
       day: "Feb 5th at 2:30pm",
-      remider: true,
+      reminder: true,
     },
     {
       id: 2,
       text: "Meeting in school",
       day: "Feb 6th at 1:30pm",
-      remider: true,
+      reminder: true,
     },
     {
-      id: 1,
+      id: 3,
       text: "Food Shopping",
       day: "Feb 5th at 2:30pm",
-      remider: false,
+      reminder: false,
     },
   ]);
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle remainders
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <AddTasks />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 };
